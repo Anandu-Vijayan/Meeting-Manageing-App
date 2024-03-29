aae2e3cb98e3295ef565171fdc14d57d181ed7a5
Anandu-Vijayan <ananduvijayan8@gmail.com>
2024-03-30 00:36:45 +0530

    Firebase Backend Completed

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -1,13 +1,57 @@
-"use client"
-import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { app } from "@/config/FirebaseConfig";
+import { LogoutLink, useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useRouter } from "next/navigation";
 
 function Dashboard() {
+  const db = getFirestore(app);
+
+  const { user } = useKindeBrowserClient();
+  const [loading,setLoading] = useState(true);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      isBusinessRegistered();
+    }
+  }, [user]);
+
+  const isBusinessRegistered = async () => {
+    try {
+      const docRef = doc(db, "Business", user.email);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+        setLoading(false)
+      } else {
+        console.log("No such document!");
+        setLoading(false)
+        router.replace('/create-business');
+      }
+    } catch (error) {
+      console.error("Error checking if business is registered:", error);
+      setLoading(false)
+      router.replace('/create-business');
+    }
+  };
+
+  if(loading){
+    {
+      return <h2>Loading .....</h2>
+    }
+
+  }
+
   return (
-    <div>Dashboard
-        <LogoutLink>Log out</LogoutLink>
+    <div>
+      <p>Dashboard</p>
+      <LogoutLink>Log out</LogoutLink>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
---
+++ config/FirebaseConfig.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey:process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "meetingapp-549b9.firebaseapp.com",
+  projectId: "meetingapp-549b9",
+  storageBucket: "meetingapp-549b9.appspot.com",
+  messagingSenderId: "474789224579",
+  appId: "1:474789224579:web:88468f4dc969f4e849bbb6",
+  measurementId: "G-WLEGCSNF4Z"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);

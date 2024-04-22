66cc2491178aadd98af6c37df328794709f25889
Anandu-Vijayan <ananduvijayan8@gmail.com>
2024-04-22 20:36:05 +0530

    Avilabilty time Working

--- app/(routes)/dashboard/availability/page.jsx
+++ app/(routes)/dashboard/availability/page.jsx
@@ -1,10 +1,42 @@
+import DaysList from "@/app/_utils/DaysList";
 import React from "react";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 function Availability() {
   return (
     <div className="p-10">
       <h2 className="font-bold text-2xl">Availability</h2>
       <hr className="my-7"></hr>
+      <div>
+        <h2 className="font-bold mt-10">Availability Days</h2>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-5 my-3">
+          {DaysList.map((item, index) => (
+            <div key={index}>
+              <h2>
+                <Checkbox />
+                {item.day}
+              </h2>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <h2 className="font-bold">Availability Time</h2>
+        <div className="flex gap-10">
+          <div className="mt-3">
+            <h2>Start Time</h2>
+            <Input type="time" />
+          </div>
+          <div className="mt-3">
+            <h2>End Time</h2>
+            <Input type="time" />
+          </div>
+        </div>
+      </div>
+      
+      <Button className="mt-10">Save</Button>
     </div>
   );
 }
